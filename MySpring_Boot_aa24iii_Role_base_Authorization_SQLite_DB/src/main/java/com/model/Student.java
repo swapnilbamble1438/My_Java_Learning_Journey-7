5c44f3964b9c0ec9cc5b8356ec81703f5bf686d5
swapnilbamble1438 <swapnilbamble1438@users.noreply.github.com>
2023-11-07 20:09:49 +0530

    MyJJ7-24

---
+++ MySpring_Boot_aa24iii_Role_base_Authorization_SQLite_DB/src/main/java/com/model/Student.java
@@ -0,0 +1,67 @@
+package com.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Student")
+public class Student {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long sid;
+	private String sname;
+	private String scity;
+
+	public Student() {
+		
+	}
+
+	public Student(Long sid, String sname, String scity) {
+		this.sid = sid;
+		this.sname = sname;
+		this.scity = scity;
+	}
+
+	public Long getSid() {
+		return sid;
+	}
+
+	public void setSid(Long sid) {
+		this.sid = sid;
+	}
+
+	public String getSname() {
+		return sname;
+	}
+
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+
+	public String getScity() {
+		return scity;
+	}
+
+	public void setScity(String scity) {
+		this.scity = scity;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [sid=" + sid + ", sname=" + sname + ", scity=" + scity + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ MySpring_Boot_aa24iii_Role_base_Authorization_SQLite_DB/src/main/java/com/service/UserService.java
@@ -0,0 +1,54 @@
+package com.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.model.User;
+import com.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	
+	public void addUser(User user)
+	{
+		userRepository.save(user);
+	}
+	
+	public List<User> getAllUsers()
+	{
+		return (List<User>) userRepository.findAll();
+		
+		
+	}
+	
+	public User getUser(int id)
+	{
+		Optional<User> user = userRepository.findById(id);
+		if(user.isPresent())
+		{
+			return user.get();
+		}
+		return null;
+	}
+	
+	public void delete(Integer id)
+	{
+		userRepository.deleteById(id);
+	}
+	
+	public User getUserByUsername(String username)
+	{
+		return userRepository.getUserByUsername(username);
+		
+	}
+	
+
+
+}
