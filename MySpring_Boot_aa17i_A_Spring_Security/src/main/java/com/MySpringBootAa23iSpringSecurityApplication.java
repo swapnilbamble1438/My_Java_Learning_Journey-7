cb947dbb61b8a87a1dd85590f8c4434400c40fe0
swapnilbamble1438 <swapnilbamble1438@users.noreply.github.com>
2023-11-07 16:17:45 +0530

    MyJJ7-1

---
+++ MySpring_Boot_aa17i_A_Spring_Security/src/main/java/com/MySpringBootAa23iSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootAa23iSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootAa23iSpringSecurityApplication.class, args);
+	}
+
+}
