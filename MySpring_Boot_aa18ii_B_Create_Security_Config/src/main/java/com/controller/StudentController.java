2175689ab59a9ba97fe1b8cdf8854abd11e461cd
swapnilbamble1438 <swapnilbamble1438@users.noreply.github.com>
2023-11-07 19:40:00 +0530

    MyJJ7-6

---
+++ MySpring_Boot_aa18ii_B_Create_Security_Config/src/main/java/com/controller/StudentController.java
@@ -0,0 +1,86 @@
+package com.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.model.Student;
+
+@RestController
+public class StudentController {
+
+	/*
+	 
+	 In Browser open
+	 http://localhost:8080/
+	then login screen will popup just put username as user
+	and password will be availabe in console
+	NOTE: if you have set username and password in
+		  application.properties then use that 
+		  username and password for loging
+	
+	then visit
+	 http://localhost:8080/students
+	 here u will get all students detail
+	 
+	 then for loging out try
+	 http://localhost:8080/logout
+	 
+	 =====================================================
+		 * Setting our own username and password in application.properties
+
+	 
+	 Now try in Postman app
+	 
+	 use GET method 
+	 try url: http://localhost:8080/students
+	 
+	 Authorization-->Basic Auth ->   username: admin , password : admin
+	 
+	 and hit send button you will get the result
+	
+	 Note: Go to header--> hidden--> authorization--> copy all text after basic  
+	 and try to decode it online by searching
+	 decode from base64 format and after decoding
+	 that code you will get your username and password.
+	 
+	 
+	*/
+	
+	private List<Student> students = new ArrayList<>();
+	
+	@GetMapping("/students")
+	public List<Student> students()
+	{		
+		Student student1 = new Student();
+		student1.setId(1);
+		student1.setFirstName("Swapnil");
+		student1.setLastName("Bamble");
+		
+		Student student2 = new Student();
+		student2.setId(2);
+		student2.setFirstName("Yash");
+		student2.setLastName("More");
+		
+		students.add(student1);
+		students.add(student2);
+		
+		return students;
+	
+		
+	}
+	
+	@PostMapping("/students")
+	public Student createStudent(Student student)
+	{
+		students.add(student);
+		return student;
+	}
+	
+	
+	
+	
+}
---
+++ MySpring_Boot_aa18ii_B_Create_Security_Config/src/main/java/com/model/Student.java
@@ -0,0 +1,65 @@
+package com.model;
+
+public class Student {
+
+	private long id;
+	private String firstName;
+	private String lastName;
+	
+	
+	public Student() {
+
+	}
+
+
+	public Student(long id, String firstName, String lastName) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+
+	public long getId() {
+		return id;
+	}
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
