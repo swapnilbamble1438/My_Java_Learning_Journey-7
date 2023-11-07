355d10b1b2317527b40f15a221ace20e97383764
swapnilbamble1438 <swapnilbamble1438@users.noreply.github.com>
2023-11-07 20:04:04 +0530

    MyJJ7-19

---
+++ MySpring_Boot_aa23i_VotingApp/src/main/java/com/config/MyConfig.java
@@ -0,0 +1,80 @@
+package com.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+@Configuration
+@EnableWebSecurity
+public class MyConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private AuthenticationSuccessHandler customSuccessHandler;
+	
+	@Bean
+	public UserDetailsService getUserDetailsServicie()
+	{
+		return new UserDetailsServiceImpl();
+	}
+
+	
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder()
+	{
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider()
+	{
+		DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+		daoAuthenticationProvider.setUserDetailsService(this.getUserDetailsServicie());
+		daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+		
+		return daoAuthenticationProvider;
+		
+	}
+
+	
+	///configure method...
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth.authenticationProvider(authenticationProvider());
+		
+		
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http
+		.authorizeRequests()
+		.antMatchers("/admin/**").hasRole("ADMIN")
+		.antMatchers("/user/**").hasRole("USER")
+		.antMatchers("/**").permitAll()
+		.and().formLogin().loginPage("/signin")
+		.loginProcessingUrl("/dologin")
+		//.defaultSuccessUrl("/user/")
+		.successHandler(customSuccessHandler)
+		.and().csrf().disable();
+	
+	}
+	
+	
+	
+	
+	
+	
+
+}
