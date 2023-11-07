1cb6e51b3e5b3efe26d2def07226cff556bcad37
swapnilbamble1438 <swapnilbamble1438@users.noreply.github.com>
2023-11-07 20:21:08 +0530

    MyJJ7-29

---
+++ MySpring_Boot_aa27_B_Email_Web_API/src/main/java/com/MySpringBootAa29EmailWebApiApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootAa29EmailWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootAa29EmailWebApiApplication.class, args);
+	}
+
+}
